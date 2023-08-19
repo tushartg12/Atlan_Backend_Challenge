1d654dc115b6df3be0766a31f597bc2fea102927
Tushar <tushartg600@gmail.com>
2023-08-19 15:47:02 -0700

    Project Completed

--- backend/src/main/java/com/atlan/backend/repository/CustomerRepository.java
+++ backend/src/main/java/com/atlan/backend/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.atlan.backend.repository;
 
 import com.atlan.backend.entity.CustomerDetails;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends MongoRepository<CustomerDetails,String> {
 }
