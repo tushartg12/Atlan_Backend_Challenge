f63779e376e50199d9c386425caa0df343d6d6d0
Tushar <tushartg600@gmail.com>
2023-09-15 00:09:53 +0530

    Credentials changed

--- backend/src/main/java/com/atlan/backend/Credentials/SmsServiceCredentials.java
+++ backend/src/main/java/com/atlan/backend/Credentials/SmsServiceCredentials.java
@@ -2,5 +2,5 @@ package com.atlan.backend.Credentials;
 
 public class SmsServiceCredentials {
     public static String ACCOUNT_SID = "ACf5b75a78be2a76a4423014ae4745a5d6";
-    public static String AUTH_TOKEN = "d86a8f4924aa09a51895a9002734b9f6";
+    public static String AUTH_TOKEN = "46cb06dd2a6a9ad584e485d142c42a3b";
 }
